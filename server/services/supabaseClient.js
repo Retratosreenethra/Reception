2a8e3fc62b3150d0fae847e6aca9868a6b47eac3
abhayrcyto <abhay@retrato.in>
2025-01-11 13:51:35 +0530

    Update supabaseClient.js

--- server/services/supabaseClient.js
+++ server/services/supabaseClient.js
@@ -4,8 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const supabaseUrl = process.env.https://pbaoxoeimnwkyowabwql.supabase.co
-const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiYW94b2VpbW53a3lvd2Fid3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3NTExODksImV4cCI6MjA1MTMyNzE4OX0.yGydSL_OczJgHZIAHNWKKk_yvZGEeSq-uSyPA-DyT2U;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Supabase URL and Key must be provided in the environment variables.');
+}
+
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
