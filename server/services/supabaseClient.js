784f2be0db4042a18db22909c0e12914eceb2d53
Retratosreenethra <retratosreenethra@gmail.com>
2025-01-11 13:07:43 +0530

    Update supabaseClient.js

--- server/services/supabaseClient.js
+++ server/services/supabaseClient.js
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = 'https://pbaoxoeimnwkyowabwql.supabase.co'
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiYW94b2VpbW53a3lvd2Fid3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3NTExODksImV4cCI6MjA1MTMyNzE4OX0.yGydSL_OczJgHZIAHNWKKk_yvZGEeSq-uSyPA-DyT2U;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
